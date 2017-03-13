752873b86ee8fc25b480516b0f24b83e1c4b5e46
Andre Broers <broersa@Andres-MacBook-Air.local>
2017-03-13 14:09:20 +0100

    latest fixes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var mustacheExpress = require('mustache-express');
 var watzdpriceApi = require('./dal/watzdpriceApi.js');
 var MyError = require('./MyError.js');
+var favicon = require('express-favicon');
 
 watzdpriceApi.init(config.watzdprice_web_api_url, config.watzdprice_web_api_key);
 
@@ -12,30 +13,120 @@ app.engine('html', mustacheExpress());
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/html');
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res, next) {
-  if (req.query.q === undefined) {
-    res.render('master', {
-      title: 'page title',
+  if (!req.query.q) {
+    return res.render('master', {
+      title: 'Home | watzdprice.nl',
       homepage: {
         items: []
       }
     });
   } else {
-    watzdpriceApi.searchProducts(0,10,req.query.q, function (err, result) {
+    watzdpriceApi.searchProducts(0,20,req.query.q, function (err, result) {
       if (err) {
-        next(new MyError('ERROR', 'home', 'Error', {q: req.query.q}, err));
+        return next(new MyError('ERROR', 'home', 'Error', {q: req.query.q}, err));
       }
-      res.render('master', {
-        title: 'page title',
+      return res.render('master', {
+        title: req.query.q + ' | watzdprice.nl',
         q: req.query.q,
-        homepage: {
+        searchresults: {
           items: result.products
         }
       });
     });
   }
 });
+
+app.get('/product/:id', function(req, res, next) {
+  if (req.params.id) {
+    watzdpriceApi.getProduct(req.params.id, function (err, result) {
+      if (err) {
+        return next(new MyError('ERROR', 'product', 'Error', {q: req.params.id}, err));
+      }
+      if (!result) {
+        return next();
+      }
+      return res.render('master', {
+        title: result.name + ' | watzdprice.nl',
+        id: req.query.id,
+        product: result
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+app.get('/about', function (req, res, next) {
+  return res.render('master', {
+    title: 'Over ons | watzdprice.nl',
+    about: {
+    }
+  });
+});
+
+app.get('/bot', function (req, res, next) {
+  return res.render('master', {
+    title: 'Bot | watzdprice.nl',
+    bot: {
+    }
+  });
+});
+
+
+app.get('/autocomplete', function(req, res, next) {
+  if (req.query.q) {
+    watzdpriceApi.getAutocomplete(req.query.q, function (err, result) {
+      if (err) {
+        console.error(JSON.stringify(err));
+        return res.status(200).end(JSON.stringify({ w: [] }));
+      }
+      return res.status(200).end(JSON.stringify({ w: result }));
+    });
+  } else {
+    return res.status(200).end(JSON.stringify({ w: [] }));
+  }
+});
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+      res.render('master', {
+        title: 'Pagina niet gevonden... | watzdprice.nl',
+        id: req.query.id,
+        notfound: {
+          url: req.url
+        }
+      });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(JSON.stringify(err));
+  res.status(500).render('master', {
+    title: 'Error | watzdprice.nl',
+    error: {
+      code: err.code,
+      message: err.message
+    }
+  });
+})
 
 app.listen(config.watzdprice_web_port);
--- src/dal/watzdpriceApi.js
+++ src/dal/watzdpriceApi.js
@@ -35,5 +35,57 @@ module.exports = {
       }
       cb(null, JSON.parse(body));
     });
+  },
+  getProduct: function (id, cb) {
+    var options = {
+      uri: url + `/getproduct?id=${encodeURIComponent(id)}`,
+      headers: {
+        accept: 'application/json',
+        apikey: key
+      }
+    }
+    request.get(options, function(err, response, body) {
+      if (err) {
+        return cb(new MyError('ERROR', 'getProduct', 'Error', {id: id}, err));
+      }
+      if (response.statusCode===400) {
+        return cb(new MyError('BADREQUEST', 'getProduct', 'Bad request', {id: id}, err));
+      }
+      if (response.statusCode===403) {
+        return cb(new MyError('FORBIDDEN', 'getProduct', 'Forbidden', {id: id}, err));
+      }
+      if (response.statusCode===500) {
+        return cb(new MyError('ERROR', 'getProduct', 'Error', {id: id}, err));
+      }
+      if (response.statusCode===204) { // not found
+        return cb(null, null);
+      }
+      cb(null, JSON.parse(body).product);
+    });
+  },
+  getAutocomplete: function (query, cb) {
+    var options = {
+      uri: url + `/getautocomplete?q=${encodeURIComponent(query)}`,
+      headers: {
+        accept: 'application/json',
+        apikey: key
+      }
+    }
+    request.get(options, function(err, response, body) {
+      if (err) {
+        return cb(new MyError('ERROR', 'getAutocomplete', 'Error', {query: query }, err));
+      }
+      if (response.statusCode===400) {
+        return cb(new MyError('BADREQUEST', 'getAutocomplete', 'Bad request', { query: query }, err));
+      }
+      if (response.statusCode===403) {
+        return cb(new MyError('FORBIDDEN', 'getAutocomplete', 'Forbidden', { query: query }, err));
+      }
+      if (response.statusCode===500) {
+        return cb(new MyError('ERROR', 'getAutocomplete', 'Error', { query: query }, err));
+      }
+      cb(null, JSON.parse(body).words);
+    });
   }
+
 }
